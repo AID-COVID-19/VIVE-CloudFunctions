81df0367d0b6861cd091a2d07f3451637692d119
Abubakar Ibrahim <abubakaribrahim08@yahoo.com>
2020-04-28 22:22:36 +0300

    Made changes to register-user and update-user actions

--- actions/user/registerUser.js
+++ actions/user/registerUser.js
@@ -5,7 +5,7 @@ const CLOUDANT_DATABASE= "vive_users";
 const CLOUDANT_USERNAME = "2ad473de-e5c5-4f3e-bbb3-7a518ae880b9-bluemix"; // if u can use environment variable good.
 const CLOUDANT_PASSWORD = "ff744127284387c282e8a91be38556501bb9b1fb9f265427ff26fd12dcb5f9e7";
 
-var cloudant;
+let cloudant = null;
 
 // Configure database connection if already not established in the container @Thanks Mofi
 if(cloudant === null){
@@ -42,7 +42,7 @@ if(cloudant === null){
   */
 function main(params) {
 	
-  var database = cloudant.db.use(CLOUDANT_DATABASE);
+  const database = cloudant.db.use(CLOUDANT_DATABASE);
   
   const { id, contributor, symptoms_start, share_covid_status, risk } = params;
   // check that params contain all the info it needs. if not send error status code 
--- actions/user/updateUser.js
+++ actions/user/updateUser.js
@@ -5,7 +5,7 @@ const CLOUDANT_DATABASE= "vive_users";
 const CLOUDANT_USERNAME = "2ad473de-e5c5-4f3e-bbb3-7a518ae880b9-bluemix"; // if u can use environment variable good.
 const CLOUDANT_PASSWORD = "ff744127284387c282e8a91be38556501bb9b1fb9f265427ff26fd12dcb5f9e7";
 
-var cloudant;
+let cloudant = null;
 
 // Configure database connection if already not established in the container @Thanks Mofi
 if(cloudant === null){
@@ -16,29 +16,26 @@ if(cloudant === null){
 }
 
 function main(params) {
-    var database = cloudant.db.use(CLOUDANT_DATABASE);
-    const { id, contributor, symptoms_start, share_covid_status, risk } = params;
-    const userToBeEdited = { id, contributor, symptoms_start, share_covid_status, risk };
+    const database = cloudant.db.use(CLOUDANT_DATABASE);
+    const { _id, _rev, contributor, symptoms_start, share_covid_status, risk } = params;
+    const userToBeEdited = { _id, _rev, contributor, symptoms_start, share_covid_status, risk };
+    console.log(userToBeEdited);
     // check that params contain all the info it needs. if not send error status code 
-    if (id === undefined || contributor === undefined || symptoms_start === undefined || share_covid_status === undefined || risk === undefined) {
+    if (_id === undefined || _rev === undefined || contributor === undefined || symptoms_start === undefined || share_covid_status === undefined || risk === undefined) {
       return {
         error: "Not Enough Arguments",
       }
     }
-    database.find({selector: {id: id}}, function(err, res){
-        if(err){
-            return {
-                error: err
-            }
-        }
-        database.insert(userToBeEdited, function(err, res){
+    return updateUser(database, userToBeEdited);
+}
+
+const updateUser = (db, userToBeEdited) => {
+    return new Promise((resolve, reject) => {
+        db.insert(userToBeEdited, function(err, res){
             if(err){
-                return {
-                    error: err
-                }
+                return reject(err);
             }
-            return res
+            return resolve(res);
         })
     })
-    
 }
