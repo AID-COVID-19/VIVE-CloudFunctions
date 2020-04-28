7b140a3974e785038fb99889e61c45e3ee9234e5
Abubakar Ibrahim <abubakaribrahim08@yahoo.com>
2020-04-28 03:14:02 +0300

    Created an update-user action

--- actions/user/registerUser.js
+++ actions/user/registerUser.js
@@ -44,16 +44,16 @@ function main(params) {
 	
   var database = cloudant.db.use(CLOUDANT_DATABASE);
   
-  const { contributor, symptoms_start, share_covid_status, risk } = params;
+  const { id, contributor, symptoms_start, share_covid_status, risk } = params;
   // check that params contain all the info it needs. if not send error status code 
-  if (contributor === undefined || symptoms_start === undefined || share_covid_status === undefined || risk === undefined) {
+  if (id === undefined || contributor === undefined || symptoms_start === undefined || share_covid_status === undefined || risk === undefined) {
     return {
       error: "Not Enough Arguments",
     }
 }
 //   console.log(params)
   
-  const data = { contributor, symptoms_start, share_covid_status, risk } // this data is what is being added to the database. structure it from params
+  const data = { id, contributor, symptoms_start, share_covid_status, risk }; // this data is what is being added to the database. structure it from params
 
     return insert(database, data);
 
